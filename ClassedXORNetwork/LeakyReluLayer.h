e8ee194f6b2b0030854b4c558d65b2408b71341a
theProgrammingBox <justinboxgea@gmail.com>
2023-02-16 17:20:22 -0800

    mid change

--- ClassedXORNetwork/LeakyReluLayer.h
+++ ClassedXORNetwork/LeakyReluLayer.h
@@ -6,36 +6,78 @@ class LeakyReluLayer : public Layer
 public:
 	LeakyReluLayer(uint32_t outputSize)
 	{
+		productMatrix = Matrix(1, outputSize);
+		activationMatrix = Matrix(1, outputSize);
 
+		productDerivativeMatrix = Matrix(1, outputSize);
 	}
-	~LeakyReluLayer() {}
+	
+	~LeakyReluLayer() override
+	{
+	}
 	
 	void Forward() override
 	{
 		cpuSgemmStridedBatched(
 			false, false,
-			outputMatrix->columns, outputMatrix->rows, inputMatrix->columns,
+			productMatrix.columns, productMatrix.rows, inputMatrix->columns,
 			&GLOBAL::ONEF,
-			weightMatrix->matrix, weightMatrix->columns, 0,
+			weightMatrix.matrix, weightMatrix.columns, 0,
 			inputMatrix->matrix, inputMatrix->columns, 0,
 			&GLOBAL::ZEROF,
-			outputMatrix->matrix, outputMatrix->columns, 0,
+			productMatrix.matrix, productMatrix.columns, 0,
 			1);
-		cpuLeakyRelu(outputMatrix->matrix, activationMatrix->matrix, outputMatrix->totalSize);
+		cpuSaxpy(productMatrix.totalSize, &GLOBAL::ONEF, biasMatrix.matrix, 1, productMatrix.matrix, 1);
+		cpuLeakyRelu(productMatrix.matrix, activationMatrix.matrix, productMatrix.totalSize);
 	}
 
 	void Backward() override
 	{
-		cpuLeakyReluDerivative(inputDerivativeMatrix->matrix, activationDerivativeMatrix->matrix, outputDerivativeMatrix->matrix, outputMatrix->totalSize);
+		cpuLeakyReluDerivative(productMatrix.matrix, activationDerivativeMatrix->matrix, productDerivativeMatrix.matrix, activationDerivativeMatrix->totalSize);
 		cpuSgemmStridedBatched(
 			true, false,
-			inputDerivativeMatrix->columns, inputDerivativeMatrix->rows, outputDerivativeMatrix->columns,
+			inputDerivativeMatrix.columns, inputDerivativeMatrix.rows, productDerivativeMatrix.columns,
 			&GLOBAL::ONEF,
-			weightMatrix->matrix, weightMatrix->columns, 0,
-			outputDerivativeMatrix->matrix, outputDerivativeMatrix->columns, 0,
+			weightMatrix.matrix, weightMatrix.columns, 0,
+			productDerivativeMatrix.matrix, productDerivativeMatrix.columns, 0,
 			&GLOBAL::ZEROF,
-			inputDerivativeMatrix->matrix, inputDerivativeMatrix->columns, 0,
+			inputDerivativeMatrix.matrix, inputDerivativeMatrix.columns, 0,
 			1);
+		cpuSgemmStridedBatched(
+			false, true,
+			weightDerivativeMatrix.columns, weightDerivativeMatrix.rows, inputMatrix->rows,
+			&GLOBAL::ONEF,
+			productDerivativeMatrix.matrix, productDerivativeMatrix.columns, 0,
+			inputMatrix->matrix, inputMatrix->columns, 0,
+			&GLOBAL::ZEROF,
+			weightDerivativeMatrix.matrix, weightDerivativeMatrix.columns, 0,
+			1);
+	}
+
+	void Update(float scalar) override
+	{
+		cpuSaxpy(weightDerivativeMatrix.totalSize, &scalar, weightDerivativeMatrix.matrix, 1, weightMatrix.matrix, 1);
+		cpuSaxpy(biasDerivativeMatrix.totalSize, &scalar, biasDerivativeMatrix.matrix, 1, biasMatrix.matrix, 1);
+	}
+
+	void AssignInputMatrix(Matrix* inputMatrix) override
+	{
+		this->inputMatrix = inputMatrix;
+	}
+	
+	Matrix* GetOutputMatrix() override
+	{
+		return &activationMatrix;
+	}
+
+	void AssignOutputDerivativeMatrix(Matrix* outputDerivativeMatrix) override
+	{
+		this->activationDerivativeMatrix = outputDerivativeMatrix;
+	}
+
+	Matrix* GetInputDerivativeMatrix() override
+	{
+		return &inputDerivativeMatrix;
 	}
 
 private:
@@ -51,9 +93,15 @@ private:
 			output[counter] = (((*(int32_t*)(input + counter) & 0x80000000) >> 31) * 0.9f + 0.1f) * gradient[counter];
 	}
 
-	Matrix inputMatrix;
+	Matrix* inputMatrix;
 	Matrix weightMatrix;
 	Matrix biasMatrix;
 	Matrix productMatrix;
 	Matrix activationMatrix;
+
+	Matrix inputDerivativeMatrix;
+	Matrix weightDerivativeMatrix;
+	Matrix biasDerivativeMatrix;
+	Matrix productDerivativeMatrix;
+	Matrix* activationDerivativeMatrix;
 };
--- ClassedXORNetwork/Network.h
+++ ClassedXORNetwork/Network.h
@@ -10,6 +10,31 @@ public:
 	~Network() { for (auto& layer : Layers) delete layer; }
 	
 	void AddLayer(Layer* layer) { Layers.push_back(layer); }
-	void Forward() { for (auto& layer : Layers) layer->Forward(); }
-	void Backward() { for (auto& layer : Layers) layer->Backward(); }
+	void Initialize(Matrix* inputMatrix, Matrix* outputDerivativeMatrix)
+	{
+		this->inputMatrix = inputMatrix;
+		this->outputDerivativeMatrix = outputDerivativeMatrix;
+
+		Layers[0]->AssignInputMatrix(inputMatrix);
+		for (int i = 1; i < Layers.size(); i++)
+			Layers[i]->AssignInputMatrix(Layers[i - 1]->GetOutputMatrix());
+
+		outputMatrix = Layers[Layers.size() - 1]->GetOutputMatrix();
+
+		Layers[Layers.size() - 1]->AssignOutputDerivativeMatrix(outputDerivativeMatrix);
+		for (int i = Layers.size() - 2; i >= 0; i--)
+			Layers[i]->AssignOutputDerivativeMatrix(Layers[i + 1]->GetInputDerivativeMatrix());
+
+		inputDerivativeMatrix = Layers[0]->GetInputDerivativeMatrix();
+	}
+	
+	Matrix* Forward() { for (auto& layer : Layers) layer->Forward(); }
+	Matrix* Backward() { for (auto& layer : Layers) layer->Backward(); }
+	void Update(float scalar) { for (auto& layer : Layers) layer->Update(scalar); }
+
+	Matrix* inputMatrix;
+	Matrix* outputMatrix;
+
+	Matrix* inputDerivativeMatrix;
+	Matrix* outputDerivativeMatrix;
 };
